19a347e685dff9fafbcb1592ac3cb3a721db10a0
Sudhanshu Ranjan <139035070+sudhanshuranja@users.noreply.github.com>
2026-03-11 15:37:17 +0530

    Time: 48 ms (68.43%), Space: 53.3 MB (92.32%) - LeetHub

---
+++ JavaScript/Medium/2637-promise-time-limit/2637-promise-time-limit-03-11-2026-15-37-16.js
@@ -0,0 +1,33 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+    return async function(...args) {
+        return new Promise((resolve, reject) => {
+            
+            // Timer for time limit
+            const timer = setTimeout(() => {
+                reject("Time Limit Exceeded");
+            }, t);
+
+            // Execute original function
+            fn(...args)
+                .then((res) => {
+                    clearTimeout(timer);
+                    resolve(res);
+                })
+                .catch((err) => {
+                    clearTimeout(timer);
+                    reject(err);
+                });
+
+        });
+    }
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
