65b3786112c18fa7b978aad24b93a7715fb7f698
Sudhanshu Ranjan <139035070+sudhanshuranja@users.noreply.github.com>
2026-03-13 09:40:12 +0530

    Time: 47 ms (53.64%), Space: 134.1 MB (9.47%) - LeetHub

---
+++ Java/Medium/0146-lru-cache/0146-lru-cache-03-13-2026-09-40-12.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+class LRUCache {
+
+    class Node {
+        int key, value;
+        Node prev, next;
+
+        Node(int k, int v) {
+            key = k;
+            value = v;
+        }
+    }
+
+    int capacity;
+    HashMap<Integer, Node> map = new HashMap<>();
+    Node head, tail;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+
+        head = new Node(0,0);
+        tail = new Node(0,0);
+
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    private void remove(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void insert(Node node) {
+        node.next = head.next;
+        node.prev = head;
+
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    public int get(int key) {
+
+        if(!map.containsKey(key))
+            return -1;
+
+        Node node = map.get(key);
+
+        remove(node);
+        insert(node);
+
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+
+        if(map.containsKey(key)) {
+            remove(map.get(key));
+        }
+
+        Node node = new Node(key, value);
+        map.put(key, node);
+        insert(node);
+
+        if(map.size() > capacity) {
+            Node lru = tail.prev;
+            remove(lru);
+            map.remove(lru.key);
+        }
+    }
+}
