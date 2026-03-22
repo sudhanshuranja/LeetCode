374c4994d902bd6d253446e3c83bd3279018d8be
Sudhanshu Ranjan <139035070+sudhanshuranja@users.noreply.github.com>
2026-03-22 14:26:02 +0530

    Time: 42 ms (74.66%), Space: 54.4 MB (38.31%) - LeetHub

---
+++ JavaScript/Medium/2694-event-emitter/2694-event-emitter-03-22-2026-14-26-05.js
@@ -0,0 +1,47 @@
+class EventEmitter {
+    
+    constructor() {
+        this.events = new Map(); // eventName -> array of callbacks
+    }
+
+    /**
+     * @param {string} eventName
+     * @param {Function} callback
+     * @return {Object}
+     */
+    subscribe(eventName, callback) {
+        if (!this.events.has(eventName)) {
+            this.events.set(eventName, []);
+        }
+
+        const callbacks = this.events.get(eventName);
+        callbacks.push(callback);
+
+        return {
+            unsubscribe: () => {
+                const index = callbacks.indexOf(callback);
+                if (index !== -1) {
+                    callbacks.splice(index, 1);
+                }
+            }
+        };
+    }
+    
+    /**
+     * @param {string} eventName
+     * @param {Array} args
+     * @return {Array}
+     */
+    emit(eventName, args = []) {
+        if (!this.events.has(eventName)) return [];
+
+        const callbacks = this.events.get(eventName);
+        const result = [];
+
+        for (let cb of callbacks) {
+            result.push(cb(...args));
+        }
+
+        return result;
+    }
+}
